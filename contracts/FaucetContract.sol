46cae76faf8a3805593f72a60a5fb3265493af6f
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-11-14 12:09:56 -0700

    added bytecode notes

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,6 +3,12 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     // storage variables
-    // recieve function
+    // recieve function called when you make a transaction tx without a specified name
+    // External function are part of the ccontract interface, i.e can be called via contracts and other tx
     receive() external payable {}
+
+    // Block info
+    // Nonce - a hash that when combined with the minHash proofs that
+    // the block has gone through proof of work(POW)
+    // 8 bytes => 64 bits
 }

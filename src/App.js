6d16c783c8e37a359ffc68a312a940f25c3a0b1e
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-11-20 15:34:12 -0700

    connected metamask and enabled ethereum

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      // with metamask we have an access to window.ethereum & to window.web3
+      // metamask injects a global API into website
+      // this API allows websites to request users, accounts, read data to blockchain
+      // sign messages and transactions
+
+      console.log(window.web3)
+      console.log(window.ethereum)
+    }
+    loadProvider() 
+  }, [])
   return (
     <>
       <div className='faucet-wrapper'>
@@ -7,6 +21,15 @@ function App() {
           <div className='balance-view is size-2'>
             Current Balance: <strong>10</strong> ETH
           </div>
+          <button
+            className='btn mr-2'
+            onClick={async () => {
+              const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+              console.log(accounts)
+            }}
+          >
+            Enable Ethereum
+          </button>
           <button className='btn mr-2'>Donate</button>
           <button className='btn'>Withdraw</button>
         </div>
@@ -16,3 +39,5 @@ function App() {
 }
 
 export default App;
+
+

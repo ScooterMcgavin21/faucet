7f73cc76bed148f4dfdc0feaeaf1429408d875d9
ScooterMcgavin21 <Scott.skrobel@gmail.com>
2021-11-25 12:27:15 -0800

    reverse engineer transaction data pt2

--- exp/index.js
+++ exp/index.js
@@ -1,10 +1,34 @@
 
 // 0xf86c01850c4b201000825208949cbfd6ebdb9cfcccd6b043f43e524583486d455e880490283b23ec8f768025a067da959a6d114d42016b5fb43ff8ae018efe6e4c784d40dfb2f2aad8fb2d4f6ca00b019b1e457b592e5bfd553e3b73742de625c7b65145494a57dbca17e5e9d842
 
-// 0xf86c01850c4b201000825208949cbfd6ebdb9cfcccd6b043f43e524583486d455e880490283b23ec8f7680
-// 25
-// a0 67da959a6d114d42016b5fb43ff8ae018efe6e4c784d40dfb2f2aad8fb2d4f6c
-// a0 0b019b1e457b592e5bfd553e3b73742de625c7b65145494a57dbca17e5e9d842
+// 0x
+// f8 = f7 + length of payload in binary [bytes] > 
+// 6c = 108 > length of payload 
+// 01 = nonce
+
+// 133  - 128 = 5bytes
+// 0x85 - 0x80 = length of the string
+// 85 0c4b201000 > extracted gas price
+
+// 0x82 - 0x80 = 2bytes
+// 82 5208 > gasLimit
+
+// 0x94 - 0x80 = 20bytes
+// 9cbfd6ebdb9cfcccd6b043f43e524583486d455e -2
+
+// 0x88 - 080 = 8bytes
+
+// 88 0490283b23ec8f76 - to
+
+// 80 - data empty string / null 
+
+// 0x25 - 1 byte is encodeing itself
+// 25 -[v]
+
+// a0 - 0x80 = 32bytes
+// a0 67da959a6d114d42016b5fb43ff8ae018efe6e4c784d40dfb2f2aad8fb2d4f6c - [r]
+
+// a0 0b019b1e457b592e5bfd553e3b73742de625c7b65145494a57dbca17e5e9d842 - [s]
 //console.log('Hello World');
 // 1 nibble = 4 bits
 // 1 byte = 8 bits 
